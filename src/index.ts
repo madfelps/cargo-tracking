df839493963212bf2b8ba3a7cf59da3f32871968
Felipe Madureira <felipemoura@usp.br>
2022-08-04 10:23:45 -0300

    error: property name does not exist on type

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { Destination } from "./Destination";
+import { Cargo } from "./Cargo";
+
+const destination = new Destination();
+console.log(destination);
+
+const cargo = new Cargo();
+console.log(cargo);
+
+console.log('hello')
