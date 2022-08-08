f48253f69e10f4127712abbd0b5fceee0e1c202e
Felipe Madureira <felipemoura@usp.br>
2022-08-08 13:46:10 -0300

    error: map doesn't show on webpage

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,11 @@ console.log(destination);
 
 const cargo = new Cargo();
 console.log(cargo);
+
+new google.maps.Map(document.getElementById('g-map'), {
+  zoom: 1.6,
+  center: {
+    lat: 0,
+    lng: 20,
+  },
+});
